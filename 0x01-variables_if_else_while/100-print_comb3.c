2b6d3e0afc115a31f3bea6044567de6eebc6bfd5
jkkaranja <karanjohnk77@gmail.com>
2023-03-20 12:33:15 -0700

    Combinations

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,19 +15,15 @@ int main(void)
 	{
 		for (j = 1 ; j < 10 ; j++)
 		{
-			if (i < j && i != j)
+			putchar(i + '0');
+			putchar(j + '0');
+			if (i + j != 17)
 			{
-			putchar (i + '0');
-			putchar (j + '0');
-			if (i+j !=17)
-				{
-				putchar (',');
-				putchar (' ');
-				}
-
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
-	putchar ('\n');
+	putchar('\n');
 	return (0);
 }
