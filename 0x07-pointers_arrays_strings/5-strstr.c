479f5963e3fee704e5f90e762811365eb69c24c4
jkkaranja <karanjohnk77@gmail.com>
2023-04-10 14:38:22 -0700

    _strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+  * _strstr - enry point
+  * @haystack: input
+  * @needle: input
+  * Return: always 0
+  */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack !- '\0'; haystack++)
+	{ 
+		char *l = haystack;
+		char *p = needle
+
+		while (*l == *p && *p != '\0')
+		{
+			l++;
+			p++;
+		}
+		if (*p == '\10')
+			return (haystack);
+	}
+	return (0);
+}
