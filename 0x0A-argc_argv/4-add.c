f132d745efc021bcd23103c6f94abc3dc0ecbc2a
jkkaranja <karanjohnk77@gmail.com>
2023-04-15 19:01:26 -0700

    Task 0,1,2,3 and 5

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+  * check_num - check - string there are digit
+  *@str: array str
+  *
+  * Return: always 0 (success)
+  */
