be8bfdfab0b6e953edfc98daa606f4e3ec23a618
jkkaranja <karanjohnk77@gmail.com>
2023-04-19 17:20:11 -0700

    Task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdio.h>
 /**
   * int_index - return index place if comparison is true, else -1
   * @array: array
@@ -16,8 +17,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]))
-			return(i);
+			return (i);
 	}
 	return (-1);
 }
-
--- 0x0F-function_pointers/2-main.c
+++ 0x0F-function_pointers/2-main.c
@@ -34,3 +34,22 @@ int abs_is_98(int elem)
 {
 	return (elem == 98 || -elem == 98);
 }
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int array[20] = {0, -98, 98, 402, 1024, 4096, -1024, -98, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 98};
+	int index;
+
+	index = int_index(array, 20, is_98);
+	printf("%d\n", index);
+	index = int_index(array, 20, abs_is_98);
+	printf("%d\n", index);
+	index = int_index(array, 20, is_strictly_positive);
+	printf("%d\n", index);
+	return (0);
+}
